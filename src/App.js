4762d09d85c22bebda64d609ebc7516340d798cb
PreethiM <preethim@cannydigit.com>
2022-01-28 17:27:24 +0530

    material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from 'react';
 import "./App.css";
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+
 import {useState} from "react";
+
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+
+
+
+import Tooltip from '@mui/material/Tooltip';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+import TextField from '@mui/material/TextField';
+
+
+
+
+
+
 
 export default function App() {
   let movie = [
@@ -54,7 +79,7 @@ export default function App() {
       summary:
         " When monsters want to get away from it all, they go to Count Dracula's (Adam Sandler) Hotel Transylvania, a lavish resort where they can be themselves without humans around to bother them. On one special weekend, Dracula invites creatures like the Invisible Man, the Mummy and others to celebrate the 118th birthday of his daughter, Mavis (Selena Gomez). However, an unforeseen complication unfolds when an ordinary human unwittingly crashes the party and falls in love with Mavis."
     },
-    {name:"Cloudy With A Chance Of Meatballs",
+    {name:" Meatballs",
     url:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/a239f11c8da6f8b476408b7d4ab7efedec9e9cad7c36fd88c6303a570ae9349b._RI_V_TTW_.jpg",
     year: " 2008",
     rating:"7.8",
@@ -64,14 +89,61 @@ export default function App() {
   }
 
   ];
+const[movieList,setMovieList]=useState(movie);
+  const[name,setName]=useState("");
+  const[url,setUrl]=useState("");
+   const[year,setYear]=useState("");
+  // const[down,setDown]=useState("");
+  // const[list,setList]=useState([]);
+      // const date={name,img,year}
+ 
+      
 
 
   return (
     <div className="movielist">
+
+
+
+<div className="movie-form">
+  <div className='card'>
+        <TextField id="outlined-basic" label="Name" variant="outlined"
+        value={name}
+       
+        onChange={(event) => setName(event.target.value)}
+/><br></br>
+<TextField id="outlined-basic" label="Image" variant="outlined"
+        value={url}
+        onChange={(event) => setUrl(event.target.value)}
+/><br></br>
+< TextField id="outlined-basic" label="year" variant="outlined"
+        value={year}
+        
+        onChange={(event) => setYear(event.target.value)}
+/><br></br></div>
+
+<div className="card1"><Button variant="outlined" onClick={()=>{
+  const newMovie={
+    name:name,
+    url:url,
+    year:year,
+  };
+  setMovieList([...movieList,newMovie]);
+}}
+>Add Movie</Button></div>
+    
+  </div>
+  {/* <movieList movies={movieList} /> */}
+{/* 
+        
+      </div> */}
+    
+  
+
       
 
       
-      {movie.map(({ name, url, summary, year, rating,download }) => (
+      {movieList.map(({ name, url, summary, year, rating,download }) => (
         <List
           Poster={name}
           pic={url}
@@ -80,21 +152,40 @@ export default function App() {
           rat={rating}
           dow={download}
         />
+
+        
       ))}
      
-     
+   
+   
     </div>
+      
+    
+    
   );
 }
+
+
 
 
 function Counter(){
   const[like,setLike]=useState(10);
   const[dislike,setDislike]=useState(10);
+  const [open, setOpen] =useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
   return(
     <div className="like-dislike-buttons">
-     < button onClick={()=>setLike(like+1)}>👍{like}</button>
-     < button onClick={()=>setDislike(dislike-1)}>👎{dislike}</button>
+      <Tooltip open={open} onClose={handleClose} onOpen={handleOpen} title="Like">
+     < button onClick={()=>setLike(like+1)}>👍{like}</button></Tooltip>
+     <Tooltip open={open} onClose={handleClose} onOpen={handleOpen} title="DisLike">
+     < button onClick={()=>setDislike(dislike-1)}>👎{dislike}</button></Tooltip>
         
      
     </div>
@@ -102,49 +193,188 @@ function Counter(){
      }
 
 
-  //   function Show(){
-  //     const [hidden, setHidden] = useState(false);
-  //     return (
-  //       <div className="but">
-          
-  //        {!hidden && <button onClick={() => setHidden(true)}>{sum}</button>}
-  //      </div>
-  //    );
-  //  }
+     function AlertDialog() {
+      const [open, setOpen] = useState(false);
     
+      const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+    
+      return (
+        <div className='link'>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Download
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+           
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+              Download Successfully !!!
+              </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose} >
+                Ok
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
+    }
 
 
+    
+    
+     
+    
+    
+    
+    function HalfRating() {
+      return (
+        <div className="star">
+        <Stack spacing={1}>
+          <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
 
-function List({ Poster, pic, sum, released, rat ,dow}) {
-  
-  const styles={ color: rat >= 8.5 ? "teal" : "crimson"};
-  const [display, setDisplay] = useState(false);
-  
-  return (
-  
-      <div className="movie-items">
-      <h1 className="head"> {Poster}</h1>
-      <img className="picture1" src={pic} alt="Poster" />
-      <p className="para1"><span className="mov-rel">Release year -</span>{released}</p>
-      <p  className="star" style={styles}><span className="mov-rat">Rating:</span>{rat}</p>
-      <a className="link" href="Download" target="blank">{dow}</a><br></br>
-      <Counter/>
-
-      
-      {/* <button onClick={Show}>Toggle summary
-      </button> */}
-      
-      <button className="but" onClick={()=>setDisplay(!display)}> Summary</button>
-      {
-        display?<p className="para"><span className="mov-sum">Movie Summary :</span>{sum}</p>:null
+        </Stack>
+        </div>
+        );
       }
 
 
-      {/* <p className="para"><span className="mov-sum">Movie Summary :</span></p> */}
 
-      {/* <Show/> */}
+      function BasicTextFields() {
+        return (
+          
+          <div className='bo'>
+            <TextField id="outlined-basic" label="Comment" variant="outlined" />
+            
+            </div>
+        );
+      }
+
+
+
+function List({ Poster, pic, sum, released, dow}) {
+  
+  // const styles={ color: rat >= 8.5 ? "teal" : "crimson"};
+  const [display, setDisplay] = useState(false);
+  
+  return (
+    
+  
+      <div className="movie-items">
+     
+    
+      <img className="picture1" src={pic} alt="Poster" />
+      
+      <h1 className="head"> {Poster}</h1>
+      <p className="para1"><span className="mov-rel">Release year -</span>{released}</p>
+      <HalfRating/>
+      {/* <p  className="star" style={styles}><span className="mov-rat">Rating:</span>{rat}</p> */}
+      {/* <a className="link" href="Download" target="blank">{dow}</a><br></br> */}
+     {/* <div className="link"> 
+     <stack>
+     <Link  href="#" underline="none">
+        {'Download'}
+        <Alert severity="success">Download Successfully !!!</Alert>
+      </Link>
+      </stack>
+      </div> */}
+      <AlertDialog/>
+      <Counter/>
+
+      
+      <div className="cd">
+      
+     <Button className="but" onClick={()=>setDisplay(!display)} variant="contained">Summary</Button>
+     <div className="cd1">
+      {
+        display?<p className="para"><span className="mov-sum">Movie Summary :</span> <Card  variant="outlined" >{sum}</Card></p>:null
+      }
+
+      
+  </div>
+  </div>
+  <BasicTextFields/>
+  {/* <AddColor/> */}
+ 
     </div>
+    
   );
 }
+
+
+  
+//   return(
+//     <div className="hii">
+//       <input  placeholder="movie-img" value={img} onChange={(e)=>setImg(e.target.value)}/>
+//       <input placeholder="movie-name" value={name} onChange={(e)=>setName(e.target.value)}/>
+//       <input placeholder="movie-year" value={year} onChange={(e)=>setYear(e.target.value)}/>
+//       <button onClick={()=>setList([...list,date])}>submit</button>
+//       {
+// list.map((a)=><div className="cool">
+ 
+//  <img className="picture" src={a.img} alt=" movie-poster"/>
+//  <h3 className="text">{a.name}</h3>
+//  <h3 className="yr">{a.year}</h3>
+
+//   </div>)
+//       }
+//     </div>
+//   );
+// }
+
+
+// // function AddColor(){
+// //   const[color,setColor] =useState(["orange","crimson","teal"]);
+// //   const[colorList,setColorList]=useState([
+// //     "teal"
+// //   ]);
+// //   const styles={background:color};
+  
+// //   return(
+// //     <div className="cl">
+// // {/* typing => onchange > setcolor > informing react that color is updated */}
+// //       <input value={color}
+// //       placeholder="enter a color"
+// //       style={styles}
+// //       onChange={(event)=>setColor(event.target.value)}
+// //       />
+// //       {/* create a copy of colorList and add new color to it */}
+// //       <button onClick={()=> setColorList([...colorList, color])}>add</button>
+// //       {
+// //         colorList.map((clr)=>(
+// //           <ColorBox cool={clr}/>
+
+// //         ))
+// //       }
+// //     </div>
+// //   );
+// // }
+
+
+// // function ColorBox({cool}){
+// //   const styles={
+// //     height:"25px",
+// //     width:"250px",
+// //     background:cool,
+// //     marginTop:"10px",
+
+// //   };
+// //   return <div style={styles}></div>;
+// // }
+
+
+// //input field
 
 
