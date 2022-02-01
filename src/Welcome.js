5d5c79810e7141c2b60fc8819506dc8731eb69e8
preethi <preethiha1799@gmail.com>
2022-02-01 18:07:25 +0530

    welcome added

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Welcome(){
     return(
-        <div>
+        <div className="boy-gif">
             <h1>WELCOME TO MY PAGE</h1>
-            <img className="boy-gif" src="https://acegif.com/wp-content/uploads/2021/4fh5wi/welcome-15.gif" alt="welcome-page"/>
+            <img  src="https://acegif.com/wp-content/uploads/2021/4fh5wi/welcome-15.gif" alt="welcome-page"/>
         </div>
     );
 }
